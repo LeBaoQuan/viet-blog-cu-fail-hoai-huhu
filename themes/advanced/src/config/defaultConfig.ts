e3195175840a7beccfed0249913bb129ec1300ed
Joey Le <bazzyjoey@gmail.com>
2023-05-23 15:27:52 +0700

    'usable'

--- themes/advanced/src/config/defaultConfig.ts
+++ themes/advanced/src/config/defaultConfig.ts
@@ -3,23 +3,33 @@ import { SiteConfig } from "./types";
 const config: SiteConfig = {
   // Website configuration
   website: {
-    title: "Gatsby Advanced Starter", // Homepage title
-    titleShort: "Advanced Blog", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
-    name: "Gatsby Advanced Starter", // Website name used for homescreen (PWA) and SEO
+    title: "Truong Hoang Huyen Minh's Blog", // Homepage title
+    titleShort: "Chay Lung Tung", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
+    name: "CHAY LUNG TUNG Blog", // Website name used for homescreen (PWA) and SEO
     description: "A GatsbyJS starter equipped with advanced features.", // Website description used for RSS feeds/meta description tag
     language: "en", // Sets the global HTML lang attribute
-    logoUrl: "/logos/logo-1024.png", // Logo used for SEO
+    logoUrl: "/logos/chaylungtung_pfp.jpg", // Logo used for SEO
     url: "https://example.com", // Domain of your website without the pathPrefix
     rss: "/rss.xml", // Path to the RSS file
-    rssTitle: "Gatsby Advanced Starter RSS Feed", // Title of the RSS feed
-    copyright: "© Copyright 2021", // Copyright string for the footer of the website and RSS feed.
+    rssTitle: "Truong Hoang Huyen Minh's Personal Blog", // Title of the RSS feed
+    copyright: "© Copyright 2023", // Copyright string for the footer of the website and RSS feed.
     themeColor: "#D83850", // Used for setting manifest and progress theme colors.
     backgroundColor: "#F7F7F7", // Used for setting manifest background color.
   },
 
   // User configuration
-  user: undefined,
-
+  user: {
+    id: "AdvancedUser", // Unique identifier of the user on the website. Used for OpenGraph SEO tags
+    firstName: "Advanced", // Used for SEO
+    lastName: "User", // Used for SEO
+    twitterName: "Vagr9K", // Twitter username used for SEO
+    linkedIn: "your-linkedin", // Used for contact information
+    github: "vagr9k", // Used for contact information
+    email: "AdvancedUser@example.com", // Used for contact information and displayed in the RSS feed
+    location: "User Location", // User location used for SEO
+    about: "A full-stack web developer looking for a challenge!", // User information used for the author section
+    avatar: "https://i.pravatar.cc/300", // User avatar used for the author section
+  },
   // Organization information used for SEO
   organization: undefined,
 
